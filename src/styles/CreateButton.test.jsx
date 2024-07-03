619d672591e7b0de886cff4ed5a561f0ed134f06
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-07-03 15:26:31 +0200

    Created test

---
+++ src/styles/CreateButton.test.jsx
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom 
+ */
+
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { CreateButton } from './CreateButton';
+
+it('The button is white', () => {
+  render(<CreateButton>tosubmit</CreateButton>)
+  
+  expect(screen.getByRole("button", {name: /tosubmit/i})).toHaveStyle({
+    color: "white",
+    backgroundColor: "#135846"
+  })
+    
+})
