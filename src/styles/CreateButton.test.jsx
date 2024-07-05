f84cc549ff065ad9199759b87e9a1db92312a198
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-07-05 22:30:54 +0200

    Added comments

--- src/styles/CreateButton.test.jsx
+++ src/styles/CreateButton.test.jsx
@@ -9,7 +9,7 @@ import { CreateButton } from './CreateButton';
 
 it('The button is white', () => {
   render(<CreateButton $backgroundColor="green">tosubmit</CreateButton>)
-  
+  //add more tests, decouple logic
   expect(screen.getByRole("button", {name: /tosubmit/i})).toHaveStyle({
     color: "white",
     backgroundColor: "green"
