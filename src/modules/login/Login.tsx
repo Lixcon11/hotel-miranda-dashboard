a95e732263e873221c34586c04fb19a39227b004
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-08-01 04:08:49 +0200

    Added dotenv

--- src/modules/login/Login.tsx
+++ src/modules/login/Login.tsx
@@ -1,17 +1,17 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom"
 import { AuthContext } from "../../components/AuthContext"
-import { useSelector } from "react-redux"
+//import { useSelector } from "react-redux"
 import { usersPageData } from "../users/functions/usersPageData"
 import React from "react"
-import { RootState, UserState } from "../../types"
+//import { RootState, UserState } from "../../types"
 import "dotenv/config";
 
 const Login = () => {
     const { authState, authDispatch } = useContext(AuthContext);
 
     usersPageData();
-    const { data } = useSelector((state: RootState) => state.users)
+    //const { data } = useSelector((state: RootState) => state.users)
 
     const submitHandler = async (e: any) => {
         e.preventDefault();
@@ -29,12 +29,14 @@ const Login = () => {
 
             if (response.ok) {
                 const data = await response.json();
-                const token = data.token;
-                
+                //const token = data.token;
+                authDispatch({type: "logIn", payload: {...data, isLoggedIn: true}})
+                /*
                 const user = data.filter((user: UserState) => user.email === email && user.password === password)[0];
                 if(user) {
                     authDispatch({type: "logIn", payload: {name: user.name, photo: user.photo, email: user.email, _id: user._id, isLoggedIn: true, token: token}})
                 }
+                */
             } else {
                 const errorData = await response.json();
                 console.log(errorData)
---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/*
+type ImportMetaEnv = {
+    readonly VITE_PUBLIC_API_DOMAIN: string;
+}
+type ImportMeta = {
+    readonly env: ImportMetaEnv;
+} 
+*/
